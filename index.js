5e2fb28d39ae1fc5608edc396651e43a04ce68b9
Maria Lantin <maria.lantin@gmail.com>
2016-04-14 23:17:24 -0400

    Finished adding options. Also made it so it only sends updated positions to all sockets except the socket that sent it. Also added a field with the id of the socket that has the update.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var io = require('socket.io')(http);
 
 var minimist = require('minimist')
 
+var host = '127.0.0.1';
+var port = 4567;
+
 var args = minimist(process.argv.slice(2), {
   string: 'host',
   alias: { h: 'help', v: 'version', p: 'port' },
@@ -13,7 +16,17 @@ var args = minimist(process.argv.slice(2), {
 for (x in args) {
 	switch (x) {
 		case 'host':
-		console.log("we have a host and it is " + args['host']);
+		host = args['host'];
+		break;
+		case 'port':
+		port = args['port'];
+		break;
+		case 'version':
+		console.log("vr-websocket version 1.0");
+		break;
+		case 'help':
+		console.log("node index.js [--host <ip>] [-p | --port <port>] [-v] [-h]");
+		console.log("The default host is 127.0.0.1. The default port is 4567.");
 		break;
 	}
 }
@@ -24,23 +37,23 @@ app.get('/', function(req,res) {
 
 io.on('connection', function(socket) {
 	console.log('a new user connected: ');
-	io.emit('new user');
+	socket.broadcast.emit('new user', {id: socket.id});
 	socket.on('gear-head',function(msg) {
 		console.log('I received headset-data: ', msg);
-		io.emit('gear-head', msg)
+		socket.broadcast.emit('gear-head', {id: socket.id, data: msg})
 	});
 	socket.on('leap-motion', function(msg) {
 		console.log("got a leap message");
 	});
 	socket.on('disconnect', function() {
 		console.log('a user disconnected')
-		io.emit('user disconnected');
+		socket.broadcast.emit('user disconnected', {id: socket.id});
 	})
 });
 
 
-// http.listen(4567, '192.168.11.34', function() {
-// 	console.log('GearVR server listening on port 4567');
-// });
+http.listen(port, host, function() {
+	console.log('GearVR server listening on ' + host + ':' + port);
+});
 
 
